d2225a03f4bb8f3889a8296bb64bb784eff59b7e
LorindeLepper <liedellaar@gmail.com>
2022-02-18 11:29:20 +0100

    initial

---
+++ resources/views/post.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>Document</title>
+    <Link rel="stylesheet" href="/app.css">
+
+</head>
+<body>
+
+    <article>
+
+       <?= $post; ?>
+
+    </article>
+
+    <a href="/">Go Back</a>
+    
+</body>
+</html>
